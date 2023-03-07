073f1fb7070c54d098ac33495538d28b4884ecab
muhammad.a.s.nugroho <muhammad.a.s.nugroho@gdplabs.id>
2023-03-07 19:29:20 +0700

    chore: add example

--- examples/main.tf
+++ examples/main.tf
@@ -10,11 +10,6 @@ terraform {
 provider "idcloudhost" {}
 
 resource "idcloudhost_s3" "test-bucket-terraform-66" {
-  name               = "test-bucket-terraform-23"
-  billing_account_id = 1200190928
-}
-
-resource "idcloudhost_s3" "test-bucket-terraform-67" {
-  name               = "test-bucket-terraform-24"
+  name               = "test-bucket-terraform-66"
   billing_account_id = 1200190928
 }
